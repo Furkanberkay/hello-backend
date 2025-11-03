aba916c826411ebab7518a214595d2ae712593ab
Furkan Berkay Özcan <furkanberkayozcan@hotmail.com>
2025-11-03 17:16:43 +0300

    init: basic HTTP server setup with /hello endpoint

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func HelloHandle(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	fmt.Println("hello backend")
+}
+
+func main() {
+	http.HandleFunc("/hello", HelloHandle)
+	fmt.Println("server listening on :8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
